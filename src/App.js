80e11a32a23f64dc82aa737505e69fbe6d0a4795
mohit03nandan <mohitnandan81825@gmail.com>
2023-01-19 11:22:34 +0530

    create note tracking home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import styles from "./App.module.css"
 
+import Home from './Components/Page/Home/Home'
 const App = () => {
   return (
-    <div>App</div>
+    <div className={styles.container} >
+          <Home/>
+    </div>
   )
 }
 
---
+++ src/Components/Page/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from "./Home.module.css"
+
+const Home = () => {
+  return (
+    <div class="card "  className={styles.main}>
+    <div class="card-body">
+      This is some text within a card body.
+    </div>
+  </div>
+  )
+}
+
+export default Home
